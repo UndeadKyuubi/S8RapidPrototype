c97050d56630f879c4f16b6bb49af089bc660764
Brandon Jacobson <bjacobson891@gmail.com>
2023-06-01 11:17:16 -0700

    Finished prototype

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # S8RapidPrototype
+
+# Group Members  
+- Brandon Jacobson  
+- Brandon Hwu  
+- Jordan Reavis
+
+# Deployment Link
+https://undeadkyuubi.github.io/S8RapidPrototype/
+
+# Sources
+- Badge: https://www.dreamstime.com/royalty-free-stock-photos-gold-badge-image5392868 - made transparent and added text via Krita
+- Forest: https://stock.adobe.com/hu/search?k=magical%20forest - tuned down saturation in Krita
+- Roly-poly: https://www.pngwing.com/en/free-png-sqmni - re-colored in Krita
+- Circle: Made by Brandon Jacobson using pixilart online
+- Snail: https://www.pngitem.com/middle/wJiTJo_snail-drawing-hd-png-download/ - re-colored in Krita  
--- screens.js
+++ screens.js
@@ -5,7 +5,7 @@ Theme: Uses visual assets to convey the themes of the game (fairies / roly polie
 Aliveness: Some sort of animation should be used to make the scene feel alive and responsive (e.g. tween-based movement)
 Controls: It should be very obvious (even without the ability to read) that you tap/click the screen to begin play.
 
-Victory screen: Some visually-satisfying scene showing a roly poly getting a badg, using motion so that the scene feels alive. 
+Victory screen: Some visually-satisfying scene showing a roly poly getting a badge, using motion so that the scene feels alive. 
 Tapping the screen cycles back to the title screen
 */
 class Title extends Phaser.Scene {
@@ -13,26 +13,55 @@ class Title extends Phaser.Scene {
         super('title');
     }
 
-    init(){
-
-    }
-
     preload(){
-        this.load.image('roly', 'assets/Roly.jpg');
+        this.load.image('roly', 'assets/Roly.png');
+        this.load.image('bg', 'assets/title.jpg');
+        this.load.image('snail', 'assets/snail.png');
+        this.load.image('circle', 'assets/circle.png');
     }
 
     create(){
         const width = this.scale.width;
         const height = this.scale.height;
+
+        let bg = this.add.image(400, 350, 'bg');
+        bg.setScale(2);
+
         let title = this.add.text(100, 100, "Roly Poly: To the End");
         
         let roly1 = this.add.image(width * 0.2, height * 0.65, 'roly');
         roly1.setScale(0.4);
+
+        let snail1 = this.add.image(width * 0.7, height * 0.65, 'snail');
+        snail1.setScale(0.4);
+
+        let circle = this.add.image(400, 540, 'circle');
+        circle.setScale(0.5);
+
+        this.tweens.add({
+            targets: circle,
+            scale: 0.8,
+            ease: 'Linear',
+            duration: 1500,
+            repeat: -1,
+            yoyo: true
+        });
+
+        this.tweens.add({
+            targets: roly1,
+            alpha: {from: 0, to: 1},
+            duration: 2000,
+            repeat: 0
+        });
+        
+        this.tweens.add({
+            targets: snail1,
+            alpha: {from: 0, to: 1},
+            duration: 2000,
+            repeat: 0
+        });
         
         this.input.on('pointerdown', () => this.scene.start('victory'));
-    }
-
-    update(){
 
     }
 }
@@ -42,20 +71,45 @@ class Victory extends Phaser.Scene {
         super('victory');
     }
 
-    init(){
-
-    }
-
     preload(){
-
+        this.load.image('badge', 'assets/badge.png');
+        this.load.image('roly', 'assets/Roly.png');
+        this.load.image('bg', 'assets/title.jpg');
+        this.load.image('snail', 'assets/snail.png')
     }
 
     create(){
+        const width = this.scale.width;
+        const height = this.scale.height;
+
+        let bg = this.add.image(400, 350, 'bg');
+        bg.setScale(2);
+        
+        let roly2 = this.add.image(width * 0.35, height * 0.65, 'roly');
+        roly2.setScale(0.4);
+    
+        let snail1 = this.add.image(500, 300, 'snail');
+        snail1.setScale(0.25);
+    
+        let badge = this.add.image(100, 100, 'badge');
+        badge.setScale(0.5);
+    
+        this.tweens.add({
+            targets: snail1,
+            alpha: {from: 0, to: 1},
+            duration: 2000,
+            repeat: 0
+        });
+
+        this.tweens.add({
+            targets: badge,
+            x: width * 0.35,
+            y: height * 0.65,
+            duration: 2000,
+            repeat: 0
+        });
+
         this.input.on('pointerdown', () => this.scene.start('title'));
-    }
-
-    update(){
-
     }
 }
 
